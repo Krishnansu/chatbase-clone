77514034f97a1279c6837eb5b945e777d84902e6
Krishnansu <21JE0483@iitism.ac.in>
2024-01-06 20:26:21 +0530

    Setup Components

---
+++ src/components/sourcesPage/index.js
@@ -0,0 +1,13 @@
+import Files from "./Files";
+import Text from "./Text";
+import Website from "./Website";
+import Qna from "./Qna";
+import Notion from "./Notion";
+
+export{
+    Files,
+    Text,
+    Website,
+    Qna,
+    Notion
+};
---
+++ src/pages/Sources.jsx
@@ -0,0 +1,12 @@
+import React from 'react' ;
+import Navbar from '../components/Navbar';
+
+const Sources = () => {
+  return (
+    <>
+        <Navbar />
+    </>
+  )
+}
+
+export default Sources ;
