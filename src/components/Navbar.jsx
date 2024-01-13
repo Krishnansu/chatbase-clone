965ba834403de245690b3c768c69f7cfe74c5818
Krishnansu <21JE0483@iitism.ac.in>
2024-01-13 23:19:05 +0530

    Fixes v8

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -24,7 +24,7 @@ const Navbar = () => {
     <button  className='mx-4 pb-2 font-medium text-gray-500 hover:text-gray-700 focus:text-gray-700 border-b-[3px] border-transparent focus:border-b-purple-600' >Chatbot</button>
     <button  className='mx-4 pb-2 font-medium text-gray-500 hover:text-gray-700 focus:text-gray-700 border-b-[3px] border-transparent focus:border-b-purple-600'>Settings</button>
     <button  className='mx-4 pb-2 font-medium text-gray-500 hover:text-gray-700 focus:text-gray-700 border-b-[3px] border-transparent focus:border-b-purple-600'>Dashboard</button>
-    <button className='mx-4 pb-2 font-medium text-gray-700 hover:text-gray-700 focus:text-gray-700 border-b-[3px] border-purple-600 focus:border-b-purple-600'>Sources</button>
+    <a href='/' className='mx-4 pb-2 font-medium text-gray-700 hover:text-gray-700 focus:text-gray-700 border-b-[3px] border-purple-600 focus:border-b-purple-600'>Sources</a>
     <button  className='mx-4 pb-2 font-medium text-gray-500 hover:text-gray-700 focus:text-gray-700 border-b-[3px] border-transparent focus:border-b-purple-600'>Integrations<span class="mx-1 inline-flex items-center rounded-md bg-purple-50 px-2 py-1 text-xs font-medium text-purple-700 ring-1 ring-inset rounded-xl ring-purple-700/10">New</span></button>
     <button  className='mx-4 pb-2 font-medium text-gray-500 hover:text-gray-700 focus:text-gray-700 border-b-[3px] border-transparent focus:border-b-purple-600'>Embed on site</button>
     <button  className='mx-4 pb-2 font-medium text-gray-500 hover:text-gray-700 focus:text-gray-700 border-b-[3px] border-transparent focus:border-b-purple-600'>Share</button>
