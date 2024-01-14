95971b6cfddfd2aba80f5dfc966f01a0dba62b87
Krishnansu <21JE0483@iitism.ac.in>
2024-01-14 11:08:47 +0530

    Fixes v12

--- backend/index.js
+++ backend/index.js
@@ -85,17 +85,18 @@ app.get("/login/:code", async (req, res) => {
   });
 
   const datum=data.results;
-  for (let i = 0; i < datum.length(); i++) {
-    if(datum[i].object==="page")
-    {
-      console.log(i," -> ",datum[i].id);
+  console.log(datum);
+  // for (let i = 0; i < datum.length(); i++) {
+  //   if(datum[i].object==="page")
+  //   {
+  //     console.log(i," -> ",datum[i].id);
       // const child = getSubBlocks(datum[i].id,resp.data.access_token);
       // if(child !== undefined)
       // {
       //     datum[i].childArray=child;
       // }
-    }
-  }
+    // }
+  // }
   
   const str=JSON.stringify(datum);
   //const str=JSON.stringify(data?.results);
